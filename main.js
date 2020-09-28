1bd65c2578740e459e629c072314fee7288ae80e
Henry Darana <henry.darana@tray.com>
2020-09-28 19:10:16 +0530

    Added Per Game For #1

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
+const table = document.querySelector('table');
+const prefix = "https://spreadsheets.google.com/feeds/list/";
+const sheetId = "1w_tkqGHWKj5UjI5EqNTKTXUTeawppDc5UJuHaVMKBn0";
+const suffix = "/od6/public/values?alt=json";
+const spreadsheetURL = prefix + sheetId + suffix;
+const tableHeads = document.querySelectorAll("th");
+
+var json_obj = JSON.parse(getData(spreadsheetURL));
+
 function getData(url) {
     var HttpReq = new XMLHttpRequest(); // a new request
     HttpReq.open("GET", url, false);
     HttpReq.send(null);
     return HttpReq.responseText;
 }
-
-const table = document.querySelector('table');
-
-const prefix = "https://spreadsheets.google.com/feeds/list/";
-const sheetId = "1w_tkqGHWKj5UjI5EqNTKTXUTeawppDc5UJuHaVMKBn0";
-const suffix = "/od6/public/values?alt=json";
-
-const spreadsheetURL = prefix + sheetId + suffix;
-
-var json_obj = JSON.parse(getData(spreadsheetURL));
 
 for (const eachManager of json_obj.feed.entry) {
     let todayPostionData = eachManager.gsx$todaysposition.$t;
@@ -23,6 +22,7 @@ for (const eachManager of json_obj.feed.entry) {
     let overallPostionData = eachManager.gsx$overallposition.$t;
     let currentPointsData = eachManager.gsx$currentpoints.$t;
     let pointsForPolePositionData = eachManager.gsx$pointsfor1.$t;
+    let pointsPerGamePolePositionData = eachManager.gsx$pergamefor1.$t;
 
     let gridTable = document.querySelector('table');
     let row = gridTable.insertRow();
@@ -33,15 +33,18 @@ for (const eachManager of json_obj.feed.entry) {
     let overallPostion = row.insertCell();
     let currentPoints = row.insertCell();
     let pointsForPolePosition = row.insertCell();
+    let pointsPerGamePolePosition = row.insertCell();
 
-    todayPostion.innerHTML = todayPostionData;
-    teamName.innerHTML = teamNameData;
-    managerName.innerHTML = managerNameData;
-    scoredToday.innerHTML = scoredTodayData;
-    overallPostion.innerHTML = overallPostionData;
-    currentPoints.innerHTML = currentPointsData;
-    pointsForPolePosition.innerHTML = pointsForPolePositionData;
-
+    if (scoredToday !== "null") {
+        todayPostion.innerHTML = todayPostionData;
+        teamName.innerHTML = teamNameData;
+        managerName.innerHTML = managerNameData;
+        scoredToday.innerHTML = scoredTodayData;
+        overallPostion.innerHTML = overallPostionData;
+        currentPoints.innerHTML = currentPointsData;
+        pointsForPolePosition.innerHTML = pointsForPolePositionData;
+        pointsPerGamePolePosition.innerHTML = pointsPerGamePolePositionData;
+    }
 }
 
 const fullTable = document.getElementsByTagName('tr');
@@ -58,7 +61,7 @@ for (const eachRow of fullTable) {
         }
 
         if (loopCounter == overallPositionIndex && parseInt(dataElement.innerHTML) < 6) {
-            console.log(dataElement);
+            // console.log(dataElement);
             eachRow.classList.add('top-5');
         }
         else if (loopCounter == overallPositionIndex && parseInt(dataElement.innerHTML) > 5) {
@@ -66,4 +69,60 @@ for (const eachRow of fullTable) {
         }
     }
 
+}
+
+
+function sortTable(ele) {
+    let table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("leadertable");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        // console.log("table.rows" + table.rows+ "rows.length "+rows.length);
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("td")[ele];
+            y = rows[i + 1].getElementsByTagName("td")[ele];
+            // Check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+/* for (let index = 0; index < tableHeads.length; index++) {
+    //tableHeads[index].addEventListener('click', sortTable(index));
+    tableHeads[index].addEventListener('click', console.log(index));
+    tableHeads[index].addEventListener('click', sortTable(3));;
+} */
+
+function action(ind) {
+    return function () {
+        console.log(ind);
+        sortTable(ind);
+    }
+};
+
+
+for (let index = 0; index < tableHeads.length; index++) {
+    if (index == 0 || index == 4)
+        tableHeads[index].addEventListener('click', action(index));
 }
