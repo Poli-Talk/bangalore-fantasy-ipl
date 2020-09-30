0ce040a735b087ee36bcc6d854072e8137d386b1
Henry Darana <henry.darana@tray.com>
2020-10-01 01:57:43 +0530

    Added Best Players Page

---
+++ bestplayer.js
@@ -0,0 +1,97 @@
+
+const prefix = "https://spreadsheets.google.com/feeds/list/";
+const sheetId = "1RTH2ju9ytDz7oB6KoljPI8mwe4gtpfu1hyfprj0jiIY";
+const suffix = "/od6/public/values?alt=json";
+const spreadsheetURL = prefix + sheetId + suffix;
+console.log(spreadsheetURL);
+const tableHeads = document.querySelectorAll("th");
+
+var json_obj = JSON.parse(getData(spreadsheetURL));
+
+function getData(url) {
+    var HttpReq = new XMLHttpRequest(); // a new request
+    HttpReq.open("GET", url, false);
+    HttpReq.send(null);
+    return HttpReq.responseText;
+}
+
+function addData(playerInfo, teamClass) {
+    if (playerInfo.gsx$postion.$t == "WICKET KEEPER") {
+        let nameClass = 'wicket-keeper-name' + " " + teamClass;
+        let priceClass = 'wicket-keeper-price' + " " + teamClass;
+        let pointsClass = 'wicket-keeper-points' + " " + teamClass;
+
+        let name = document.getElementsByClassName(nameClass);
+        let price = document.getElementsByClassName(priceClass);
+        let points = document.getElementsByClassName(pointsClass);
+
+        name[0].innerHTML = playerInfo.gsx$name.$t;
+        price[0].innerHTML = playerInfo.gsx$price.$t;
+        points[0].innerHTML = playerInfo.gsx$points.$t;
+    }
+
+    if (playerInfo.gsx$postion.$t == "BATSMAN") {
+        let nameClass = 'batsman-name' + " " + teamClass;
+        let priceClass = 'batsman-price' + " " + teamClass;
+        let pointsClass = 'batsman-points' + " " + teamClass;
+
+        let name = document.getElementsByClassName(nameClass);
+        let price = document.getElementsByClassName(priceClass);
+        let points = document.getElementsByClassName(pointsClass);
+
+        name[0].innerHTML = playerInfo.gsx$name.$t;
+        price[0].innerHTML = playerInfo.gsx$price.$t;
+        points[0].innerHTML = playerInfo.gsx$points.$t;
+    }
+
+    if (playerInfo.gsx$postion.$t == "ALL ROUNDER") {
+        let nameClass = 'all-rounder-name' + " " + teamClass;
+        let priceClass = 'all-rounder-price' + " " + teamClass;
+        let pointsClass = 'all-rounder-points' + " " + teamClass;
+
+        let name = document.getElementsByClassName(nameClass);
+        let price = document.getElementsByClassName(priceClass);
+        let points = document.getElementsByClassName(pointsClass);
+
+        name[0].innerHTML = playerInfo.gsx$name.$t;
+        price[0].innerHTML = playerInfo.gsx$price.$t;
+        points[0].innerHTML = playerInfo.gsx$points.$t;
+    }
+
+    if (playerInfo.gsx$postion.$t == "BOWLER") {
+        let nameClass = 'bowler-name' + " " + teamClass;
+        let priceClass = 'bowler-price' + " " + teamClass;
+        let pointsClass = 'bowler-points' + " " + teamClass;
+
+        let name = document.getElementsByClassName(nameClass);
+        let price = document.getElementsByClassName(priceClass);
+        let points = document.getElementsByClassName(pointsClass);
+
+        name[0].innerHTML = playerInfo.gsx$name.$t;
+        price[0].innerHTML = playerInfo.gsx$price.$t;
+        points[0].innerHTML = playerInfo.gsx$points.$t;
+    }
+}
+
+for (const playerInfo of json_obj.feed.entry) {
+
+    switch (playerInfo.gsx$abbr.$t) {
+        case "CSK": addData(playerInfo, 'csk');
+            break;
+        case "DC": addData(playerInfo, 'dc');
+            break;
+        case "KXIP": addData(playerInfo, 'kxip');
+            break;
+        case "KKR": addData(playerInfo, 'kkr');
+            break;
+        case "MI": addData(playerInfo, 'mi');
+            break;
+        case "RR": addData(playerInfo, 'rr');
+            break;
+        case "RCB": addData(playerInfo, 'rcb');
+            break;
+        case "SRH": addData(playerInfo, 'srh');
+            break;
+    }
+
+}
